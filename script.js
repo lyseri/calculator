402bdd59df8fc3c9f3284726ebc8b3732f4b5239
lyseri <cbmer@tutanota.com>
2022-04-19 14:40:59 -0600

    Add functionality to ANS and point keys

--- script.js
+++ script.js
@@ -31,22 +31,27 @@ function condenseInput(input) {
     solveEquation = [];
 
     for (let i in input) {
-        if (typeof input[i] == 'number') {
+        if (typeof input[i] == 'number' || input[i] == '.') {
             numHolder += input[i];
             if (i == input.length - 1) {
-                solveEquation.push(parseInt(numHolder));
+                solveEquation.push(parseFloat(numHolder));
                 numHolder = '';
             }
         } else {
-            solveEquation.push(parseInt(numHolder));
+            solveEquation.push(parseFloat(numHolder));
             solveEquation.push(input[i]);
             numHolder = '';
         }
     }
+    console.log(solveEquation);
     return solveEquation;
 } 
 
 currentEquation = [];
+previousAnwser = [];
+
+// For saving if current num has a point
+pointChecker = false;
 
 const display = document.querySelector('#display');
 
@@ -59,13 +64,17 @@ const clear = document.querySelector('#clear');
 const backspace = document.querySelector('#backspace');
 const anwser = document.querySelector('#anwser');
 const equals = document.querySelector('#equals');
+const point = document.querySelector('#point')
 
 // Collects inputs in currentEquation plus adds them to display
 for (let i = 0; i < inputButtons.length; i++) {
     inputButtons[i].addEventListener('click', () => {
         display.textContent += inputButtons[i].textContent;
         currentEquation.push(parseInt(inputButtons[i].textContent))
+
+        console.log(currentEquation);
     });
+    
 }
 
 // Pushes currentNum to currentEquation and checks for valid preceeding character (a number)
@@ -74,24 +83,52 @@ for (let i = 0; i < operatorButtons.length; i++) {
         if (typeof currentEquation[currentEquation.length-1] == 'number') {
             currentEquation.push(operatorButtons[i].textContent);
             display.textContent += operatorButtons[i].textContent;
+            pointChecker = false;
         } else {
             return;
         }
+
+        console.log(currentEquation);
     });
 }
+
+// Adds point input
+point.addEventListener('click', () => {
+    if (pointChecker == false) {
+        display.textContent += point.textContent;
+        currentEquation.push(point.textContent);
+        pointChecker = true; 
+    } else {
+        return;
+    }
+
+    console.log(currentEquation);
+});
+
+// Inputs previous anwser
+anwser.addEventListener('click', () => {
+    for (let i in previousAnwser) {
+        currentEquation.push(previousAnwser[i]);
+        display.textContent += previousAnwser[i];
+    }
+    console.log(currentEquation);
+});
 
 // Clears display and currentEquation
 clear.addEventListener('click', () => {
     currentEquation = [];
     display.textContent = '';
+    pointChecker = false;
+
+    console.log(currentEquation);
 });
 
 // Removes last input
 backspace.addEventListener('click', () => {
     last = currentEquation[currentEquation.length - 1];
-
     currentEquation.pop(last);
     display.textContent = display.textContent.slice(0,-1)
+
     console.log(currentEquation);
 });
 
@@ -133,10 +170,22 @@ equals.addEventListener('click', () => {
         // Sets result as display and starts as first term in currentEquation
         display.textContent = solveEquation[0];
         currentEquation = [];
-        string = solveEquation[0] + '';
+        previousAnwser = [];
 
+        // Adds anwser to previousAnwser and currentEquation as single char indexes
+        string = solveEquation[0] + '';
         for (let i = 0; i < string.length; i++) {
-            currentEquation.push(parseInt(string[i]));
+            if (string[i] == '.') {
+                currentEquation.push(string[i]);
+                previousAnwser.push(string[i]);
+            } else {
+                currentEquation.push(parseInt(string[i]));
+                previousAnwser.push(parseInt(string[i]));
+            }
+            
         }
     }
+
+    console.log(solveEquation[0]);
+    console.log(currentEquation);
 });
